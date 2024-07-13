08c74ca0ae510ec19a910891b1da847aada840b2
Harsh-Jaiswal24 <harsh4jaiswal@gmail.com>
2024-07-13 22:08:16 +0530

    Initial commit

---
+++ Day-1.js
@@ -0,0 +1,47 @@
+// ACTIVITY 1
+
+var a;
+a=10;
+console.log(a);
+
+let b;
+b="Hello ! I am Harsh Jaiswal";
+console.log(b);
+
+// ACTIVITY 2
+
+const C=true;
+console.log(C);
+
+//ACTIVITY 3
+
+let n=83846;
+console.log(typeof(n));
+let s="THIS IS STRING";
+console.log(typeof(s));
+let t=true;
+console.log(typeof(t));
+const p=["hh","fff","dds",{a1: "djdj", a2: "d4d4", a3: 43},"hghj"]  // this is array
+console.log(typeof(p));
+
+const o={                  //this is OBJECT  
+    s1 : 2,
+    s2 : ["new", "old", 34],
+    s3 : "hello"
+
+}
+console.log(typeof(o));
+
+//ACTIVITY 4
+
+let no= 4;
+console.log(no);
+no=5;
+console.log(no);
+
+// ACTIVITY 5
+
+const to=34;
+to=45;
+console.log(to);
+
