506b40832e3c22a85eee382c9e8da5e870a45308
Harsh-Jaiswal24 <harsh4jaiswal@gmail.com>
2024-07-14 21:37:49 +0530

    Day-2 Completed

---
+++ Day-2/Day-2.js
@@ -0,0 +1,54 @@
+//ACTIVITY-1
+
+let a= 20;
+let b=5;
+console.log(`${a+b} ${a-b} ${a*b} ${a/b} ${a%b}`);
+
+//ACTIVITY-2
+
+let d=500;
+console.log(d+=10);
+
+let e= 900;
+console.log(e-=100);
+
+// ACTIVITY-3
+
+let p=5344;
+let o=5344;
+
+
+console.log(o>p);
+console.log(o<p);
+
+console.log(o>=p);
+console.log(o<=p);
+console.log(o=p);  // if equal it will return value of p if not ture also return valur of p;
+
+console.log(5 == '5'); // true (type coercion) // first convert into same datatype than compare
+console.log(5 === '5'); // false (different types)   //do not change datatype direct compare
+
+console.log(0 == false); // true (type coercion)
+console.log(0 === false); // false (different types)
+let dd= 'hello';
+let pp='djdj'
+
+console.log(dd===pp);
+console.log(dd==pp);
+ 
+//ACTIVITY-4
+
+let z=40;
+let y=40;
+
+console.log(z==y && z===y);
+
+let w=10;
+let t=24;
+console.log(w==t || w<t);
+console.log(w!=t);
+
+//ACTIVITY-5
+
+let ss=20;
+console.log( ss>=0? "Positive" : "Negative");
